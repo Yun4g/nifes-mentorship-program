80d05f8849928b81bc37c06e7908cfa5de4b4d05
Yun4g <delightvincent487@gmail.com>
2025-03-10 22:09:09 +0100

    nifes project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <div>
+        <h1 className=' text-slate-900'>hii</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
