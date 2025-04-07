765fa798332f9e6c7850204761d8bf097ddeff22
Yun4g <delightvincent487@gmail.com>
2025-04-07 21:36:15 +0100

    push

--- src/component/Landing page/homecomponents/Potential.jsx
+++ src/component/Landing page/homecomponents/Potential.jsx
@@ -9,10 +9,10 @@ const Potential = () => {
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-orange-50 '>
           <h2 className='text-[12px] sm:text-2xl lg:text-4xl font-bold'>Unlock Your Potential with <br /> Expert Guidance</h2>
           
-          <button className=' mt-4'>
+          <button className=' mt-7'>
             <Link 
               to='/sign-up' 
-              className='mx-auto text-orange-500 text-[12px] sm:text-base md:block border bg-orange-50 flex border-orange-50 px-6 py-3 sm:px-10 sm:py-4 rounded-full shadow-lg hover:bg-orange-500 hover:text-white transition-all duration-300 ease-in-out transform hover:scale-110'
+              className='mx-auto text-orange-500 text-[14px] sm:text-base md:block border bg-orange-50 flex border-orange-50 px-4 py-3 sm:px-10 sm:py-4 rounded-full shadow-lg hover:bg-orange-500 hover:text-white transition-all duration-300 ease-in-out transform hover:scale-110'
             >
               Get Started
             </Link> 
