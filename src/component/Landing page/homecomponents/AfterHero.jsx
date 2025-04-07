fcb0f982d16966595784b24c0a00664e1e04ccd2
Yun4g <delightvincent487@gmail.com>
2025-04-07 19:13:13 +0100

    updated landing page

--- src/component/Landing page/homecomponents/AfterHero.jsx
+++ src/component/Landing page/homecomponents/AfterHero.jsx
@@ -5,7 +5,7 @@ import { CircleDotDashed } from 'lucide-react';
 const AfterHero = () => {
   return (
     <div className=' '>
-      <div className='relative  flex items-center justify-center h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+      <div className='relative pb-20 '>
         <img className='mt-20 h-15 w-full' src={asset.section} alt="" />
         <div className='absolute md:top-5 sm:top-2 top-1 w-full flex items-center justify-center text-[10px] sm:text[16px] md:text-[24px] md:px-3 px-2 sm:px-5 text-orange-50 font-bold'> 
           <CircleDotDashed className='w-4 mr-2 sm:mr-6 md:mr-2'/>
