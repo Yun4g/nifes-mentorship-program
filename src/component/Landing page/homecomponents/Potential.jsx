2e14a54ff2d2a21875e68111ff7e03f31597c71a
Yun4g <delightvincent487@gmail.com>
2025-04-07 21:30:50 +0100

    push

--- src/component/Landing page/homecomponents/Potential.jsx
+++ src/component/Landing page/homecomponents/Potential.jsx
@@ -4,13 +4,20 @@ import { asset } from '@/assets/assets'
 import { Link } from 'react-router-dom'
 
 const Potential = () => {
+  const navigate = useNavigate()
   return (
     <div className='py-20 '>
       <div className="relative bg-[url('/Section5.png')] bg-center bg-cover sm:h-80 h-40 w-100 rounded-xl mx-6 sm:mx-10">
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-orange-50 '>
         <h2 className='text-[12px] sm:text-2xl lg:text-4xl font-bold'>Unlock Your Potential with <br /> Expert Guidance</h2>
         
-      <Button as={Link} to="/sign-up" className='text-sm sm:text-lg sm:mt-10 mt-5 p-4 bg-orange-50 text-orange-500 rounded-xl'> Join Us Now</Button> 
+      <Button
+        as={Link}
+        to="/sign-up"
+        className="text-sm sm:text-lg sm:mt-10 mt-5 px-6 py-3 bg-orange-50 text-orange-500 font-semibold rounded-full shadow-lg hover:bg-orange-100 hover:shadow-xl transition-all duration-300"
+      >
+        Join Us Now
+      </Button>
       </div>
       
     </div>
