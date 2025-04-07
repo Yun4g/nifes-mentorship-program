f03496851fc17ccec1b5c5a760204dbccc5dae48
Yun4g <delightvincent487@gmail.com>
2025-04-07 18:51:25 +0100

    updated landing page

--- src/component/Landing page/homecomponents/AfterHero.jsx
+++ src/component/Landing page/homecomponents/AfterHero.jsx
@@ -10,7 +10,7 @@ const AfterHero = () => {
         <div className='absolute md:top-4 sm:top-2 top-1 w-full flex items-center justify-center text-[10px] sm:text[16px] md:text-[24px] md:px-3 px-2 sm:px-5 text-orange-50 font-bold'> 
           <CircleDotDashed className='w-4 mr-2 sm:mr-6 md:mr-2'/>
           <div className='mr-2'>
-          Your Mentor, Your Future 
+          Your Mentorship, Your Future 
           </div>
           <CircleDotDashed className='w-4 mr-2 sm:mr-4 sm:ml-6'/> 
           <div className='mr-2'>Connect, Grow, Achieve </div>
