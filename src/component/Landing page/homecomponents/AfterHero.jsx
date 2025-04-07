74f0407751a3717af1e244a1bf8b977bab00c7fd
Yun4g <delightvincent487@gmail.com>
2025-04-07 19:11:20 +0100

    update

--- src/component/Landing page/homecomponents/AfterHero.jsx
+++ src/component/Landing page/homecomponents/AfterHero.jsx
@@ -7,7 +7,7 @@ const AfterHero = () => {
     <div className=' '>
       <div className='relative  flex items-center justify-center h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
         <img className='mt-20 h-15 w-full' src={asset.section} alt="" />
-        <div className='absolute md:top-7 sm:top-2 top-1 w-full flex items-center justify-center text-[10px] sm:text[16px] md:text-[24px] md:px-3 px-2 sm:px-5 text-orange-50 font-bold'> 
+        <div className='absolute md:top-5 sm:top-2 top-1 w-full flex items-center justify-center text-[10px] sm:text[16px] md:text-[24px] md:px-3 px-2 sm:px-5 text-orange-50 font-bold'> 
           <CircleDotDashed className='w-4 mr-2 sm:mr-6 md:mr-2'/>
           <div className='mr-2'>
           Your Mentorship, Your Future 
