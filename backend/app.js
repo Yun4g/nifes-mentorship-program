d5df08d58d33a3caeef1ba7d76ebaa082689773a
Yun4g <delightvincent487@gmail.com>
2025-04-09 07:43:41 +0100

    push

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userRoutes from './routes/users.js';
+import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/auth.js';
 import emailVerificationRoutes from './routes/emailVerificationRoutes.js';
 import paymentRoutes from './routes/payments.js';
@@ -29,4 +29,6 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); 
+});
+
+export default app;
