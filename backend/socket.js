591b8993f994409a77f17e89f4c567dc4945dce0
MichAdek23 <adelekem600@gmail.com>
2025-04-16 14:26:57 +0100

    First commit

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ import Conversation from './models/Conversation.js';
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: process.env.CLIENT_URL || 'https://leap-on-mentorship-program-xkjq.vercel.app/',
+      origin: process.env.CLIENT_URL || 'http://localhost:5173/',
       methods: ['GET', 'POST'],
       credentials: true,
       allowedHeaders: ['Authorization', 'Content-Type'],
